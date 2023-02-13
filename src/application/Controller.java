785b0a9903315600c46612f90b51af1328a5076d
monabbirhasan <phpjsmonabbirhasan@gmai.com>
2023-02-13 19:05:13 +0600

    Javafx-ImageView

---
+++ src/application/Controller.java
@@ -0,0 +1,119 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class Controller {
+	
+	@FXML
+	
+	ImageView myimage;
+	Button btn1;
+	Button btn2;
+	Button btn3;
+	Button btn4;
+	Button btn5;
+	Button btn6;
+	Button btn7;
+	Button btn8;
+	Button btn9;
+	Button btn10;
+	Button btn11;
+	Button btn12;
+	Button btn13;
+	Button btn14;
+	Button btn15;
+	Button btn16;
+	Button btn17;
+	Button btn18;
+	Button btn19;
+	Button btn20;
+	
+	
+	Image img1=new Image(getClass().getResourceAsStream("Images/4.jpg"));
+	Image img2=new Image(getClass().getResourceAsStream("Images/4.png"));
+	Image img3=new Image(getClass().getResourceAsStream("Images/5.jpg"));
+	Image img4=new Image(getClass().getResourceAsStream("Images/5.png"));
+	Image img5=new Image(getClass().getResourceAsStream("Images/6.jpg"));
+	Image img6=new Image(getClass().getResourceAsStream("Images/6.png"));
+	Image img7=new Image(getClass().getResourceAsStream("Images/7.jpg"));
+	Image img8=new Image(getClass().getResourceAsStream("Images/mobile5.jpg"));
+	Image img9=new Image(getClass().getResourceAsStream("Images/mobile6.jpg"));
+	Image img10=new Image(getClass().getResourceAsStream("Images/mobile1.jpg"));
+	Image img11=new Image(getClass().getResourceAsStream("Images/mobile2.jpg"));
+	Image img12=new Image(getClass().getResourceAsStream("Images/mobile3.jpg"));
+	Image img13=new Image(getClass().getResourceAsStream("Images/mobile4.jpg"));
+	Image img14=new Image(getClass().getResourceAsStream("Images/mobile5.jpg"));
+	Image img15=new Image(getClass().getResourceAsStream("Images/mobile6.jpg"));
+	Image img16=new Image(getClass().getResourceAsStream("Images/mobile7.jpg"));
+	Image img17=new Image(getClass().getResourceAsStream("Images/mobile8.jpg"));
+	Image img18=new Image(getClass().getResourceAsStream("Images/mobile9.jpg"));
+	Image img19=new Image(getClass().getResourceAsStream("Images/mobile10.jpg"));
+	Image img20=new Image(getClass().getResourceAsStream("Images/5.jpg"));
+	
+	
+	public void display1() {
+		myimage.setImage(img1);
+	}
+	public void display2() {
+		myimage.setImage(img2);
+	}
+	public void display3() {
+		myimage.setImage(img3);
+	}
+	public void display4() {
+		myimage.setImage(img4);
+	}
+	public void display5() {
+		myimage.setImage(img5);
+	}
+	public void display6() {
+		myimage.setImage(img6);
+	}
+	public void display7() {
+		myimage.setImage(img7);
+	}
+	public void display8() {
+		myimage.setImage(img8);
+	}
+	public void display9() {
+		myimage.setImage(img9);
+	}
+	public void display10() {
+		myimage.setImage(img10);
+	}
+	public void display11() {
+		myimage.setImage(img11);
+	}
+	public void display12() {
+		myimage.setImage(img12);
+	}
+	public void display13() {
+		myimage.setImage(img13);
+	}
+	public void display14() {
+		myimage.setImage(img14);
+	}
+	public void display15() {
+		myimage.setImage(img15);
+	}
+	public void display16() {
+		myimage.setImage(img16);
+	}
+	public void display17() {
+		myimage.setImage(img17);
+	}
+	public void display18() {
+		myimage.setImage(img18);
+	}
+	public void display19() {
+		myimage.setImage(img19);
+	}
+	public void display20() {
+		myimage.setImage(img20);
+	}
+	
+	
+}
---
+++ src/module-info.java
@@ -0,0 +1,5 @@
+module ImageViews {
+	requires javafx.controls;
+	
+	opens application to javafx.graphics, javafx.fxml;
+}
